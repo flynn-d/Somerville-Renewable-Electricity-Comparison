8183f80572c1fa0db07e44761174e03d6edfeb0a
Jackson Clawson <jclaw3456@gmail.com>
2021-06-03 19:30:14 -0400

    renaming some things, making error and success clearer

--- js/script.js
+++ js/script.js
@@ -4,30 +4,30 @@
     var RPS_YEAR = 10; // TODO
 
     function init() {
-        window.CCA = {};
-        CCA.output = function (d) {
+        window.Model = {};
+        Model.output = function (d) {
             var bill5 = d.custcharge + d.deliverycharges * d.kwhslider + d.rate5 * d.kwhslider;
             var bill100 = d.custcharge + d.deliverycharges * d.kwhslider + d.rate100 * d.kwhslider;
             var co2_5 = d.kwhslider * (RPS_YEAR + 5) / 100 * d.co2avoid;
             var co2_100 = d.kwhslider * d.co2avoid;
 
-            console.log(bill5, bill100, co2_5, co2_100);
-            document.querySelector('#output').textContent = "bill5: " + bill5 + ", bill100: " + bill100 + ", co2_5: " + co2_5 + ", co2_100: " + co2_100;
+            return { bill5: bill5, bill100: bill100, co2_5: co2_5, co2_100: co2_100 };
         };
     }
 
     function finalize() {
-        window.App = {};
-        App.form = document.querySelector('form');
-        App.inputs = App.form.querySelectorAll('input');
-        App.fields = [];
-        App.inputs.forEach(function (el) {
-            App.fields.push(el.getAttribute('name'));
+        window.VM = {};
+        VM.form = document.querySelector('form');
+        VM.inputs = VM.form.querySelectorAll('input');
+        VM.fields = [];
+        VM.inputs.forEach(function (el) {
+            VM.fields.push(el.getAttribute('name'));
         });
-        App.parseAndValidate = function () {
+        VM.parseAndValidate = function () {
             var validation = [];
             var valid = true;
-            App.inputs.forEach(function (el) {
+            VM.clearState();
+            VM.inputs.forEach(function (el) {
                 var field = {
                     fieldName: el.getAttribute('name')
                 };
@@ -52,19 +52,26 @@
                 validation.forEach(function (field) {
                     if (!field.valid) {
                         // show errors
+                        document.querySelector('#error').innerHTML += "<br>" + field.fieldName + ": " + field.value + " is invalid";
                     }
                 });
                 return false;
             }
         };
+
+        VM.clearState = function () {
+            document.querySelector('#error').innerHTML = "";
+            document.querySelector('#success').innerHTML = "";
+        };
         
 
-        App.form.querySelector('button').addEventListener('click', function (ev) {
+        VM.form.querySelector('button').addEventListener('click', function (ev) {
             ev.preventDefault();
-            var data = App.parseAndValidate();
+            var data = VM.parseAndValidate();
 
             if (data) {
-                CCA.output(data);
+                var d = Model.output(data);
+                document.querySelector('#success').innerHTML = "<br>bill5: " + d.bill5 + "<br>bill100: " + d.bill100 + "<br>co2_5: " + d.co2_5 + "<br>co2_100: " + d.co2_100;
             }
 
         });
